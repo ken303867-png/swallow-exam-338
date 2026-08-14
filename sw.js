cb8e929de4112d8156f7420c1256bb6da211611e
ken303867-png <ken303867@gmail.com>
2026-08-14 20:54:30 +0900

    Cache answer confirmation script

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
-const CACHE='swallow-exam-338-v1';
-const ASSETS=['./','./index.html','./styles.css','./app-loader.js','./app.part1','./app.part2','./data.json','./manifest.webmanifest','./icon.svg'];
+const CACHE='swallow-exam-338-v2-confirm';
+const ASSETS=['./','./index.html','./styles.css','./app-loader.js','./app.part1','./app.part2','./answer-confirm.js','./data.json','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('swallow-exam-338-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
